0c330a396e8bc0796dce303ad5c38a0ee6257b0f
Michael Willemse <michaekl.willemse@xt-i.com>
2016-04-16 13:42:46 +0200

    poc of $RefParser

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var $RefParser = require('json-schema-ref-parser');
 
-$RefParser.dereference("poc-data/parent.json")
+$RefParser.dereference("sample/apigateway/swagger.json")
     .then(function(api) {
-        console.log(api);
+        console.log(JSON.stringify(api, null, '\t'));
     })
     .catch(function(err) {
         console.log(err);
